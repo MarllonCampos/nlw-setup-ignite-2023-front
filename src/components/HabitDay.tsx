1d2844c9f013ba73447ea437097065cecb115c33
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-01-23 03:27:17 -0300

    feat: creating a logic to put or remove the colors on habitday

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -3,7 +3,10 @@ interface HabitProps {
 }
 export const HabitDay = ({ completed }: HabitProps) => {
   const color = ["bg-zinc-900", "bg-violet-900", "bg-violet-800", "bg-violet-700", "bg-violet-600", "bg-violet-500"];
+  const habitColor = false && color[completed];
   return (
-    <div className={`${color[completed]} h-10 w-10 bg-zinc-900 border- border-zinc-800 rounded-lg`}>{completed}</div>
+    <div className={`${habitColor ? habitColor : ""} h-10 w-10 bg-zinc-900 border- border-zinc-800 rounded-lg`}>
+      {completed}
+    </div>
   );
 };
